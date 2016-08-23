d18d64c1c2f7ece8e228259c2d67db23231a71aa
Methodician <jacobgjohnston@gmail.com>
2016-08-22 17:26:59 -0700

    Initial Commit
    Just getting started...

---
+++ src/CSS-ColorMixer/wwwroot/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@
+Component({
+    selector: 'app',
+    template: '<h2>My App Component</h2><h3>{{ test }}<h3>'
+})
+export class AppComponent {
+    test = 'This is a test string';
+}
